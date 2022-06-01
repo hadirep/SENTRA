c37dc7ced7e7c386ba3f3e6ff7ea5341ca2ba62c
Hadi Rahmah Esa Putra <hadiesarahma@gmail.com>
2022-06-01 07:06:14 +0700

    Merubah initialRoute

--- front-end/Sentra/lib/main.dart
+++ front-end/Sentra/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: HomePage.routeName,
+      initialRoute: LoginPage.routeName,
       routes: {
         LoginPage.routeName: (context) => const LoginPage(),
         RegisterPage.routeName: (context) => const RegisterPage(),
