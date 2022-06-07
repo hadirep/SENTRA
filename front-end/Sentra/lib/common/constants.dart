48805061a62b57dfb0808773cd3ca186569cdd60
Hadi Rahmah Esa Putra <73008840+hadirep@users.noreply.github.com>
2022-06-07 23:30:11 +0700

    Migrasi dari BLoC ke Provider

--- front-end/Sentra/lib/common/constants.dart
+++
@@ -1,2 +0,0 @@
-const String BASE_IMAGE_KESENIAN_URL = 'https://sentra.dokternak.id/public/kesenians/';
-const String BASE_IMAGE_DOC_KESENIAN_URL = 'https://sentra.dokternak.id/public/dokumentasiKesenians/';
