7b06ec52ce415854d8a229015209ec1a407e3589
fadhelaps <aqiel727@gmail.com>
2022-06-11 14:58:25 +0700

    dark mode : preference provider

---
+++ front-end/Sentra/lib/presentation/provider/preference_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sentra/data/preferences/preference_helper.dart';
+import 'package:sentra/common/style.dart';
+
+class PreferenceProvider extends ChangeNotifier{
+  PreferencesHelper preferencesHelper;
+
+  PreferenceProvider({required this.preferencesHelper}){
+    _getTheme();
+    _getDailyRestaurantPreferences();
+  }
+
+  bool _isDarkTheme = false;
+  bool get isDarkTheme => _isDarkTheme;
+
+  bool _isDailyRestaurantActive = false;
+  bool get isDailyRestaurantActive => _isDailyRestaurantActive;
+
+  ThemeData get themeData => _isDarkTheme ? darkTheme : lightTheme;
+
+  void _getTheme() async {
+    _isDarkTheme = await preferencesHelper.isDarkTheme;
+    notifyListeners();
+  }
+
+  void _getDailyRestaurantPreferences() async{
+    _isDailyRestaurantActive = await preferencesHelper.isDailyRestaurantActive;
+    notifyListeners();
+  }
+
+  void enableDarkTheme(bool value) {
+    preferencesHelper.setDarkTheme(value);
+    _getTheme();
+  }
+
+  void enableDailyRestaurant(bool value) {
+    preferencesHelper.setDailyRestaurant(value);
+    _getDailyRestaurantPreferences();
+  }
+}
