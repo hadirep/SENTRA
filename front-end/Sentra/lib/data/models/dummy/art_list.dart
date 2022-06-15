b5437d6d12d8dfa97275681ccd5ed4be4b132fd3
fadhelaps <aqiel727@gmail.com>
2022-06-15 22:30:24 +0700

    favorite list (not fixed yet)
    masih no data karena belum ada favorite button

--- front-end/Sentra/lib/data/models/dummy/art_list.dart
+++ front-end/Sentra/lib/data/models/dummy/art_list.dart
@@ -1,4 +1,4 @@
-class ArtList {
+class ArtListDummy {
   final String provience;
   final String name;
   final String id;
@@ -6,7 +6,7 @@ class ArtList {
   final String description;
   final String image;
 
-  const ArtList({
+  const ArtListDummy({
     required this.id,
     required this.provience,
     required this.name,
@@ -17,7 +17,7 @@ class ArtList {
 }
 
 var dataArtList = [
-  const ArtList(
+  const ArtListDummy(
     id: "1",
     provience: "Bali",
     name: "Tari Kecak",
@@ -27,7 +27,7 @@ var dataArtList = [
         "\n Tarian ini sering dipuji oleh wisatawan asing gara-gara keunikannya, walau cuma berbunyi “cak-cak-cak” tetapi beberapa group membunyikannya dengan ketukan berbeda-beda, saat kamu mendengarkan sekelompok laki-laki yang membunyikan kata “cak” kamu seolah mendengarkan ribuan orang yang membunyikan kata “cak”. Inilah keunikan tari kecak. Tari ini disajikan sangat sederhana, tetapi suasana mistisnya masih selamanya ada.",
     image: "assets/seni/1_bali_tari_kecak.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "2",
     provience: "Papua",
     name: "Tari Musyoh",
@@ -36,7 +36,7 @@ var dataArtList = [
         "\n Jika tari Musyoh dilakukan untuk pengusiran arwah atau yang bertujuan sakral, maka yang menarikan adalah penari pria. Tarian Musyroh yang ditujukan untuk menyambut tamu atau memberikan penghormatan kepada pendatang, maka yang menarikan adalah penari wanita.",
     image: "assets/seni/2_papua_tari_musyoh.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "3",
     provience: "Aceh",
     name: "Tari Saman",
@@ -46,7 +46,7 @@ var dataArtList = [
         "\n Pada 24 November 2011, Tari Saman ditetapkan UNESCO sebagai Daftar Representatif Budaya Takbenda Warisan Manusia dalam sidang ke-6 Komite Antar-Pemerintah untuk Perlindungan Warisan Budaya Tak Benda UNESCO di Bali.",
     image: "assets/seni/3_aceh_tari_saman.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "4",
     provience: "Jawa Timur",
     name: "Tari Reog Ponorogo",
@@ -55,7 +55,7 @@ var dataArtList = [
         "\nSeni Reog Ponorogo merupakan cipta kreasi Indonesia, yang kaya akan budaya. Seni ini terbentuk karena adanya aliran kepercayaan secara turun temurun. Upacaranya pun menggunakan syarat-syarat yang tidak mudah bagi orang awam untuk memenuhinya, tanpa adanya garis keturunan yang jelas.",
     image: "assets/seni/4_jawa_timur_tari_reog_ponorogo.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "5",
     provience: "Jawa Barat",
     name: "Tari Jaipong",
@@ -64,7 +64,7 @@ var dataArtList = [
         "\nTari ini dahulu dijadikan sebagai hiburan rakyat, tari jaipong merupakan sebuah inovasi yang dilakukan oleh seorang seniman asal Karawang, tari ini merupakan gabungan dari pencak silat, wayang golek, topeng banjet, ketuk tilu, tarling, dan tepak topeng.",
     image: "assets/seni/5_jawa_barat_tari_jaipong.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "6",
     provience: "Lampung",
     name: "Tari Bendana",
@@ -75,7 +75,7 @@ var dataArtList = [
         "\nTari ini diyakini identik dengan agama Islam karena mencerminkan masyarakat Lampung yang ramah dan terbuka sebagai simbol persahabatan dan pergaulan.",
     image: "assets/seni/6_lampung_tari_bandana.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "7",
     provience: "Jawa Barat",
     name: "Tari Gambyong",
@@ -85,7 +85,7 @@ var dataArtList = [
         "\nMeskipun banyak macamnya, tarian ini memiliki dasar gerakan yang sama, yaitu gerakan tarian tayub. Pada dasarnya, gambyong dicipta untuk penari tunggal, namun sekarang lebih sering dibawakan oleh beberapa penari dengan menambahkan unsur blocking panggung sehingga melibatkan garis dan gerak yang serba besar.",
     image: "assets/seni/7_jawa_tengah_tari_gambyong.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "8",
     provience: "Banten",
     name: "Tari Topeng",
@@ -94,7 +94,7 @@ var dataArtList = [
         "\nSiapa empu pencipta tarian ini tak diketahui. Kemunculannya pun ada banyak versi. Salah satunya menyebut tari topeng sudah dikenal pada masa Majapahit. Jacob Sumardjo dalam Arkeologi Budaya Indonesia menyebut Raja Hayam Wuruk menari dengan topeng terbuat dari emas. Setelah jatuhnya Majapahit.",
     image: "assets/seni/8_banten_tari_topeng.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "9",
     provience: "Yogyakarta",
     name: "Tari Serimpi",
@@ -105,7 +105,7 @@ var dataArtList = [
         "\nTari ini ditampilkan dengan gerakan lemah gemulai dan diiringi alunan gamelan. Tema tari serimpi adalah pertentangan antara nafsu dan akal serta benar dan salah. Dalam perkembangannya, tari serimpi banyak ditampilkan untuk menyambut tamu agung.",
     image: "assets/seni/9_diy_tari_serimpi.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "10",
     provience: "Riau",
     name: "Tari Zipin",
@@ -116,7 +116,7 @@ var dataArtList = [
         "\nKarena masuk di lingkungan istana dengan cepat tari Zapin berakulturasi dengan budaya lokal. Dalam setiap gerakannya disisipkan nilai-nilai dan norma Melayu. Akhirnya tari ini kerap ditampilkan dalam acara seremonial kerajaan.",
     image: "assets/seni/10_riau_tari_zipin.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "11",
     provience: "NTT",
     name: "Tari Cerana",
@@ -125,7 +125,7 @@ var dataArtList = [
         "\nKemudian Tamu yang datang akan mengunyah sirih dan pinang yang telah diberikan sebagai simbol bahwa tamu tersebut juga menyambut baik apa yang diberikan oleh masyarakat, sehingga akan terjalin suatu hubungan yang baik diantara mereka.",
     image: "assets/seni/11_ntt_tari_cerana.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "12",
     provience: "Sumatera Utara",
     name: "Tari Tor Tor",
@@ -135,7 +135,7 @@ var dataArtList = [
         "\nDi mana mempunyai tempat dan kedudukan yang penting dalam kehidupan masyarakat adat. Dalam setiap pelaksanaan adat, Tortor selalu digelar. Dalam pelaksanaan adat, baik yang berhubungan dengan kematian maupun peristiwa sukacita, tortor selalu diadakan. Tari Tortor merupakan bagian penting dalam masyarakat adat Batak, yang memiliki nilai budaya dan nilai spiritual.",
     image: "assets/seni/12_sumatera_utara_tari_tor_tor.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "13",
     provience: "Sulawesi Selatan",
     name: "Tari Kipas Pakarena",
@@ -146,7 +146,7 @@ var dataArtList = [
         "\nTarian daerah ini dulunya, dimainkan di dalam istana Kerajaan Gowa oleh seorang putri-putri bangsawan. Tari Kipas Pakarena tersebut juga digunakan sebagai bentuk pelengkap maupun suatu hal wajib dipertunjukkan ketika ada upacara adat, pesta kerajaan, dan lain sebagainya.",
     image: "assets/seni/13_sulawesi_selatan_tari_kipas_pakarena.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "14",
     provience: "Sumatera Barat",
     name: "Tari Piring",
@@ -157,7 +157,7 @@ var dataArtList = [
         "\nGerakan-gerakan tersebut merupakan gambaran peristiwa kegiatan masyarakat dalam bekerja. Tari piring diiringi oleh musik tradisional yang disebut talempong. Musik talempong terdiri atas enam buah talempong, satu buah gong kecil, satu buah tambua, satu buah botol dan sejenis kerincing.",
     image: "assets/seni/14_sumatera_barat_tari_piring.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "15",
     provience: "Kalimantan Timur",
     name: "Tari Gantar",
@@ -167,7 +167,7 @@ var dataArtList = [
         "\nMenurut kepercayaan masyarakat, jika darah kerbau yang disembelih menyembur dengan sangat deras, berarti hasil panen akan berlimpah. Mengutip Kementerian Pendidikan dan Kebudayaan, Tari Gantar melambangkan kegembiraan dan juga keramahan Suku Dayak dalam menyambut tamu yang datang berkunjung, baik sebagai wisatawan, investor, atau para tamu yang dihormati. Bahkan, mereka diajak menari bersama para penari.",
     image: "assets/seni/15_kalimatan_timur_tari_gantar.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "16",
     provience: "DKI Jakarta",
     name: "Tari Ondel-Ondel",
@@ -176,7 +176,7 @@ var dataArtList = [
         "\nBoneka laki-laki mengenakan topeng berwarna merah, sedangkan yang perempuan berwarna putih. Ondel-ondel biasanya ditampilkan untuk menyambut tamu dan memeriahkan pesta-pesta rakyat. Konon ondel-ondel dipercaya sebagai penolak bala. Tari Ondel-ondel tidak mengusung boneka khas Betawi ini sebagai properti, tetapi menggantinya dengan tampah kecil berhiaskan wajah khas ondel-ondel yang dibawa oleh masing-masing penari. Terkadang menggunakan media lain seperti karton, dan ada juga yang tidak mengenakannya sama sekali.",
     image: "assets/seni/16_dki_jakarta_tari_ondel_ondel.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "17",
     provience: "Sumatera Selatan",
     name: "Tari Gending Sriwijaya",
@@ -187,7 +187,7 @@ var dataArtList = [
         "\nTarian ini digagas dari tahun 1942 hingga 1943 dan sempat terkendala akibat kondisi politik di tanah air. Baru pada bulan Oktober 1943 ditindaklanjuti ketika O.M. Shida menunjuk Nungtjik A.R. yang merupakan Wakil Kepala Hodohan pengganti M.J. Su’ud.",
     image: "assets/seni/17_sumatera_selatan_tari_gending_sriwijaya.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "18",
     provience: "Kalimantan Barat",
     name: "Tari Pedang Mualang",
@@ -199,7 +199,7 @@ var dataArtList = [
         "\nMembicarakan kesenian tari di Suku Dayak, ada satu tarian yang begitu menarik dan akan dibahas disini, namanya adalah Tari Pedang Mualang.",
     image: "assets/seni/18_kalimantan_barat_tari_pedang_mualang.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "19",
     provience: "Bangka Belitung",
     name: "Tari Campak",
@@ -209,7 +209,7 @@ var dataArtList = [
         "\nTarian ini berkembang pada masa pendudukan bangsa Portugis di Bangka Belitung. Hal ini bisa dilihat dari beberapa ragam pada tari Campak antara lain akordion dan pakaian pada penari perempuan yang sangat kental dengan gaya Eropa nya.",
     image: "assets/seni/19_bangka_belitung_tari_campak.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "20",
     provience: "Maluku",
     name: "Tari Yerik",
@@ -219,7 +219,7 @@ var dataArtList = [
         "\nSalah satunya di negara Turki saat ada festival budaya yang dihadiri oleh 7 negara yaitu Indonesia, Turki, India, Sri Langka, Belgia, Korea Utara dan Ukraina.",
     image: "assets/seni/20_maluku_tari_yerik.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "21",
     provience: "Kalimantan Selatan",
     name: "Tari Topeng Banjar",
@@ -228,7 +228,7 @@ var dataArtList = [
         "\nTari topeng ini diketahui sudah tumbuh dan berkembang sejak Kerajaan Negara Dipa. Bentuk topeng Banjar terdiri dari beberapa jenis seperti Gunung Sari, Patih, Panji, Batarakala (Sangkala/Gajah Barung), Pantul, Tambam, Pamambi, Pamimdu, Kalana, Ranggajiwa, dan lain - lain. Perhelatan tari topeng banjar masih dilakukan sebagai ungkapan rasa syukur, mengharap keselamatan dari gangguan roh jahat.",
     image: "assets/seni/21_kalimantan_selatan_tari_topeng_banjar.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "22",
     provience: "Kalimantan Tengah",
     name: "Tari Giring-Giring",
@@ -240,7 +240,7 @@ var dataArtList = [
         "\nBiasanya tari Giring-Giring ditampilkan untuk menyambut kedatangan tamu-tamu istimewa. Bahkan terkadang dijadikan tarian pergaulan para muda-muda di Kalimantan.",
     image: "assets/seni/22_kalimantan_tengah_tari_giring_giring.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "23",
     provience: "NTB",
     name: "Tari Gandrung",
@@ -249,7 +249,7 @@ var dataArtList = [
         "\nTarian ini adalah salah satu bentuk kebudayaan dari Suku Osing yang merupakan penduduk asli Banyuwangi. Tarian ini dipentaskan dalam bentuk berpasangan antara perempuan (penari gandrung) dan laki-laki (pemaju) yang dikenal dengan paju.",
     image: "assets/seni/23_ntb_tari_gandrung.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "24",
     provience: "Sulawesi Utara",
     name: "Tari Maengket",
@@ -259,7 +259,7 @@ var dataArtList = [
         "\nDilansir dari situs Kementerian Pendidikan dan Kebudayaan Republik Indonesia, kata maengket berasal dari tradisi budaya gotong royong di Minahasa dalam kegiatan sehari-hari, terutama saat bercocok tanam. Seiring berkembangnya zaman, maengket diartikan sebagai seni bernyanyi sambil menari dengan mengungkapkan sastra daerah yang dilakukan oleh sekelompok orang.",
     image: "assets/seni/24_sulawesi_utara_tari_maengket.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "25",
     provience: "Kalimantan Utara",
     name: "Tari Mance",
@@ -269,7 +269,7 @@ var dataArtList = [
         "\nTari Mance merupakan tarian Kalimantan Utara yang sangat digemari sebagaian besar pemuda di daerah Bulungan.",
     image: "assets/seni/25_kalimantan_utara_tari_mance.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "26",
     provience: "Banten",
     name: "Debus",
@@ -279,7 +279,7 @@ var dataArtList = [
         "\nSeni debus perlahan kemudian berkembang sebagai media untuk memompa semangat rakyat Banten dalam menghadapi penjajahan Belanda pada masa pemerintahan Sultan Ageng Tirtayasa.",
     image: "assets/seni/26_banten_debus.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "27",
     provience: "Jawa Barat",
     name: "Wayang Golek",
@@ -290,7 +290,7 @@ var dataArtList = [
         "\nPertunjukkan wayang golek dilakukan secara tatap langsung, artinya ada tatap langsung antara dalang dengan penonton. Dalang bertindak sebagai pencerita maupun sebagai pemain wayang.",
     image: "assets/seni/27_jawa_barat_wayang_golek.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "28",
     provience: "Jawa Timur",
     name: "Wayang Kulit",
@@ -300,7 +300,7 @@ var dataArtList = [
         "\nDimainkan oleh seorang dalang, wayang kulit membawa cerita-cerita dengan pesan yang berasal dari kepercayaan dan budaya setempat mengenai budi pekerti luhur, atau bahkan berupa kritik sosial.",
     image: "assets/seni/28_jawa_timur_wayang_kulit.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "29",
     provience: "Sumatra Barat",
     name: "Pencak Silat",
@@ -311,7 +311,7 @@ var dataArtList = [
         "\nBanyaknya tokoh Islam yang mengembangkan pencak silat karena catatan sejarah pencak silat banyak diwarnai di kaum penyebar agama Islam pada abad ke-14 di Nusantara. Kala itu pencak silat diajarkan bersama-sama dengan pelajaran agama di surau atau pesantren. Silat menjadi bagian dari latihan spiritual.",
     image: "assets/seni/29_jawa_barat_pencak_silat.jpg",
   ),
-  const ArtList(
+  const ArtListDummy(
     id: "30",
     provience: "Jawa Barat",
     name: "Pertunjukan Angklung",
